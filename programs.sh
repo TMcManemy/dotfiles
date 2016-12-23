7a31ffea3e10651d90335ee898ef903a577c43a5
Tim McManemy <tim.mcmanemy@gmail.com>
2016-12-23 17:26:08 -0600

    Add Docker (and Docker Compose) to programs

--- programs.sh
+++ programs.sh
@@ -1,3 +1,5 @@
+sudo apt update
+
 sudo apt install chrome
 sudo apt install dropbox
 sudo apt install keepass
@@ -17,3 +19,15 @@ sudo apt install nodejs
 # vim
 sudo apt install vim
 sudo apt install ctags
+
+# docker
+sudo apt install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt update
+sudo apt install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt install docker-engine
+
+# docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
